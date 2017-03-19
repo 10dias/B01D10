e3cfef0206528b65a3f28d4e8a7eaebc1ea3516d
thassiov <tvmcarvalho@gmail.com>
2017-03-18 22:45:46 -0300

    Fazendo request para a API

--- bmw.js
+++ bmw.js
@@ -3,3 +3,13 @@
     * @desc Retornar BMW
     * @license MIT
     */
+
+'use strict';
+
+const https = require('https');
+
+const API = 'https://fipe-parallelum.rhcloud.com/api/v1/carros/marcas/7/modelos';
+
+https.get(API, function(res) {
+    console.log(res);
+});
