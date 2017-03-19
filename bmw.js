edb037adc5a6049a99260fd82112b82c34af6aa1
thassiov <tvmcarvalho@gmail.com>
2017-03-18 22:49:30 -0300

    Imprimindo os dados recebidos

--- bmw.js
+++ bmw.js
@@ -11,5 +11,14 @@ const https = require('https');
 const API = 'https://fipe-parallelum.rhcloud.com/api/v1/carros/marcas/7/modelos';
 
 https.get(API, function(res) {
-    console.log(res);
+    let bmw = '';
+
+    res.on('data', function(d) {
+        bmw += d;
+    });
+
+    res.on('end', function() {
+        bmw = JSON.parse(bmw);
+        console.log(bmw);
+    });
 });
